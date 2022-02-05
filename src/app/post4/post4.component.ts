c1ea4171605d2912e8a1025fbeeb8cfdaa6b9c58
kate <ksidorenko@3knbh.com>
2022-02-05 22:38:20 +0200

    13 lessons (basics, 1-way data/event binding)

---
+++ src/app/post4/post4.component.ts
@@ -0,0 +1,22 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-post4',
+    template: `
+        <div class="post4">
+            <h2>Post title</h2>
+            <p>Lorem ipsum blab bla bla</p>
+        </div>
+    `,
+    styles: [`
+        .post4 {
+            padding: 5rem;
+            border: 2px solid black;
+        }
+        h2 {
+            font-size: 1rem;
+        }
+    `]
+})
+
+export class Post4Component {}
